3f96b413b598871945fee8f2d5ae40f3cebea368
tonijigan <100856397+tonijigan@users.noreply.github.com>
2023-06-14 13:23:19 +0300

    Добавьте файлы проекта.

---
+++ Napilnik2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Napilnik2
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+
+        public void Fire(Player player)
+        {
+            player.TakeDamage(_damage);
+            RemoveBullet();
+        }
+
+        private void RemoveBullet()
+        {
+            int minBullets = 0;
+            int oneBullet = 1;
+
+            if (_bullets > minBullets)
+                _bullets -= oneBullet;
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public void TakeDamage(int damage)
+        {
+            int minHealth = 0;
+
+            if (_health > minHealth)
+                _health -= damage;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            if (player != null)
+                _weapon.Fire(player);
+        }
+    }
+}
